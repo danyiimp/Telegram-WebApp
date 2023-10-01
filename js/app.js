d7e207b8241d75fefc2aba136b186583eb9266d4
Daniil <danymstudy@gmail.com>
2023-10-01 22:00:06 +0300

    Fix

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ const sendButton = document.getElementById("confirm_button")
 
 function requestOptions(dataToSend) {
     return {
-        method: 'POST', // Метод запроса
+        method: 'POST',
         headers: {
-            'Content-Type': 'application/json' // Заголовок для указания типа данных
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify(dataToSend) // Преобразуйте данные в JSON и установите их в теле запроса
+        body: JSON.stringify(dataToSend)
     }
 };
 
@@ -25,7 +25,6 @@ function fetchData(dataToSend) {
         .catch(error => console.error(error));
 };
 
-
 sendButton.addEventListener("click", () => {
     let activeSlide = document.getElementsByClassName("swiper-slide-active")[0];
     if (!tg.initDataUnsafe.query_id) {
@@ -36,63 +35,3 @@ sendButton.addEventListener("click", () => {
             data = {queryId: queryId, scenario: activeSlide.id}
             fetchData(data);
 });
-
-// sendStandard.addEventListener('click', () => {
-//     if (!tg.initDataUnsafe.query_id) {
-//         alert('WebViewQueryId not defined');
-//         return;
-//     }
-//     queryId = tg.initDataUnsafe.query_id
-//     data = {queryId: queryId, scenario: "standard"}
-//     fetchData(data);
-// });
-
-// sendBanquet.addEventListener('click', () => {
-//     if (!tg.initDataUnsafe.query_id) {
-//         alert('WebViewQueryId not defined');
-//         return;
-//     }
-//     queryId = tg.initDataUnsafe.query_id
-//     data = {queryId: queryId, scenario: "banquet"};
-//     fetchData(data);
-// });
-
-// sendCinema.addEventListener('click', () => {
-//     if (!tg.initDataUnsafe.query_id) {
-//         alert('WebViewQueryId not defined');
-//         return;
-//     }
-//     queryId = tg.initDataUnsafe.query_id
-//     data = {queryId: queryId, scenario: "cinema"};
-//     fetchData(data);
-// });
-
-// sendConference.addEventListener('click', () => {
-//     if (!tg.initDataUnsafe.query_id) {
-//         alert('WebViewQueryId not defined');
-//         return;
-//     }
-//     queryId = tg.initDataUnsafe.query_id
-//     data = {queryId: queryId, scenario: "conference"};
-//     fetchData(data);
-// });
-
-// sendParty.addEventListener('click', () => {
-//     if (!tg.initDataUnsafe.query_id) {
-//         alert('WebViewQueryId not defined');
-//         return;
-//     }
-//     queryId = tg.initDataUnsafe.query_id
-//     data = {queryId: queryId, scenario: "party"};
-//     fetchData(data);
-// });
-
-// sendBuffet.addEventListener('click', () => {
-//     if (!tg.initDataUnsafe.query_id) {
-//         alert('WebViewQueryId not defined');
-//         return;
-//     }
-//     queryId = tg.initDataUnsafe.query_id
-//     data = {queryId: queryId, scenario: "buffet"};
-//     fetchData(data);
-// });
--- js/swiper.js
+++ js/swiper.js
@@ -1,30 +1,20 @@
 const swiper = new Swiper(".swiper", {
-    loop: true,
-    preventInteractionOnTransition: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    loop: true
   });
 
-// slider = document.getElementsByClassName("swiper")[0];
+slider = document.getElementsByClassName("swiper")[0];
 
-// function handleTouchStart(event) {
-//   // Запоминаем начальное положение касания
-//   this.startY = event.touches[0].clientY;
-// };  
+function handleTouchStart(event) {
+  this.startY = event.touches[0].clientY;
+};  
 
-// // Обработчик события touchmove
-// function handleTouchMove(event) {
-//   // Вычисляем разницу между начальным и текущим положением касания
-//   var deltaY = this.startY - event.touches[0].clientY;
+function handleTouchMove(event) {
+  var deltaY = this.startY - event.touches[0].clientY;
 
-//   // Если разница больше определенного значения (например, 10 пикселей), предотвращаем прокрутку страницы
-//   if (Math.abs(deltaY) > 5) {
-//     event.preventDefault();
-//   }
-// }
+  if (Math.abs(deltaY) > 5) {
+    event.preventDefault();
+  }
+}
 
-// // Добавляем обработчики событий touchstart и touchmove к слайдеру
-// slider.addEventListener('touchstart', handleTouchStart, false);
-// slider.addEventListener('touchmove', handleTouchMove, false);
+slider.addEventListener('touchstart', handleTouchStart, false);
+slider.addEventListener('touchmove', handleTouchMove, false);
