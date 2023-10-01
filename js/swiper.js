f7ef881afe8f1e737bb990a85e1dad17ddd225ba
Daniil <danymstudy@gmail.com>
2023-10-02 00:58:46 +0300

    Fix

--- js/swiper.js
+++ js/swiper.js
@@ -2,22 +2,6 @@ const swiper = new Swiper(".swiper", {
     loop: true
   });
 
-// slider = document.getElementsByClassName("swiper")[0];
-
-// function handleTouchStart(event) {
-//   this.startY = event.touches[0].clientY;
-// };  
-
-// function handleTouchMove(event) {
-//   var deltaY = this.startY - event.touches[0].clientY;
-
-//   if (Math.abs(deltaY) > 10) {
-//     event.preventDefault();
-//   }
-// }
-
-slider.addEventListener('touchstart', handleTouchStart, false);
-slider.addEventListener('touchmove', handleTouchMove, false);
 document.addEventListener("touchmove", function (event) {
   event.preventDefault();
 },
