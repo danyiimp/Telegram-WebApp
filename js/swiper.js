7c54972c9978a2e8e8883567dce494bd09dd52c2
Daniil <danymstudy@gmail.com>
2023-10-01 19:31:00 +0300

    Фиксит скролл

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper(".swiper", {
     loop: true,
+    preventInteractionOnTransition: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
