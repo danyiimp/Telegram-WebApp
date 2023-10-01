dde57d0bb493f599d049eb62344305423c0febda
Daniil <danymstudy@gmail.com>
2023-10-01 20:24:30 +0300

    Фиксит скролл

--- js/swiper.js
+++ js/swiper.js
@@ -6,3 +6,26 @@ const swiper = new Swiper(".swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+slider = document.getElementsByClassName("swiper")[0];
+console.log(slider)
+
+function handleTouchStart(event) {
+  // Запоминаем начальное положение касания
+  this.startY = event.touches[0].clientY;
+};  
+
+// Обработчик события touchmove
+function handleTouchMove(event) {
+  // Вычисляем разницу между начальным и текущим положением касания
+  var deltaY = this.startY - event.touches[0].clientY;
+
+  // Если разница больше определенного значения (например, 10 пикселей), предотвращаем прокрутку страницы
+  if (Math.abs(deltaY) > 5) {
+    event.preventDefault();
+  }
+}
+
+// Добавляем обработчики событий touchstart и touchmove к слайдеру
+slider.addEventListener('touchstart', handleTouchStart, false);
+slider.addEventListener('touchmove', handleTouchMove, false);
