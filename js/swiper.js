a30371f2c6c848a58f8371415d459d2b51a5d94b
Daniil <danymstudy@gmail.com>
2023-10-01 21:47:54 +0300

    Fix

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper(".swiper", {
     loop: true,
+    speed: 1000,
     preventInteractionOnTransition: true,
     navigation: {
       nextEl: ".swiper-button-next",
@@ -7,24 +8,24 @@ const swiper = new Swiper(".swiper", {
     },
   });
 
-// slider = document.getElementsByClassName("swiper")[0];
+slider = document.getElementsByClassName("swiper")[0];
 
-// function handleTouchStart(event) {
-//   // Запоминаем начальное положение касания
-//   this.startY = event.touches[0].clientY;
-// };  
+function handleTouchStart(event) {
+  // Запоминаем начальное положение касания
+  this.startY = event.touches[0].clientY;
+};  
 
-// // Обработчик события touchmove
-// function handleTouchMove(event) {
-//   // Вычисляем разницу между начальным и текущим положением касания
-//   var deltaY = this.startY - event.touches[0].clientY;
+// Обработчик события touchmove
+function handleTouchMove(event) {
+  // Вычисляем разницу между начальным и текущим положением касания
+  var deltaY = this.startY - event.touches[0].clientY;
 
-//   // Если разница больше определенного значения (например, 10 пикселей), предотвращаем прокрутку страницы
-//   if (Math.abs(deltaY) > 5) {
-//     event.preventDefault();
-//   }
-// }
+  // Если разница больше определенного значения (например, 10 пикселей), предотвращаем прокрутку страницы
+  if (Math.abs(deltaY) > 5) {
+    event.preventDefault();
+  }
+}
 
-// // Добавляем обработчики событий touchstart и touchmove к слайдеру
-// slider.addEventListener('touchstart', handleTouchStart, false);
-// slider.addEventListener('touchmove', handleTouchMove, false);
+// Добавляем обработчики событий touchstart и touchmove к слайдеру
+slider.addEventListener('touchstart', handleTouchStart, false);
+slider.addEventListener('touchmove', handleTouchMove, false);
